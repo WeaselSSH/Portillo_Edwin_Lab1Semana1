eb4044f430ee0625e3d3648fae309a4b8e4b85d3
WeaselSSH <weaselssh@gmail.com>
2025-04-25 14:23:51 -0700

    Programa 2 calcular cuota mensual y total a pagar
    Se le pide al usuario el monto del prestamo, el plazo de pago, el porcentaje de interes mensual, la comisión por cuota y el seguro mensual para calcular la cuota de pago mensual y total a pagar

--- portillo_edwin_lab1semana1/src/portillo_edwin_lab1semana1/Portillo_Edwin_Cuotas.java
+++ portillo_edwin_lab1semana1/src/portillo_edwin_lab1semana1/Portillo_Edwin_Cuotas.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package portillo_edwin_lab1semana1;
+import java.util.Scanner;
 
-/**
- *
- * @author edwin
- */
 public class Portillo_Edwin_Cuotas {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Favor ingrese el monto del prestamo: ");
+        double montoPrestamo = scanner.nextDouble(); 
+        System.out.print("Favor ingrese el plazo de pago (en meses): ");
+        int plazoPago = scanner.nextInt(); 
+        System.out.print("Favor ingrese el porcentaje de interes mensual aplicado al prestamo (sin poner el símbolo de %): ");
+        double interesMensual = scanner.nextDouble() / 100;
+        System.out.print("Favor ingrese la comisión por cuota: ");
+        double comisionCuota = scanner.nextDouble();
+        System.out.print("Favor ingrese el porcentaje de seguro mensual (sin poner el símbolo de ): ");
+        double porcentajeSeguro = scanner.nextDouble() / 100;
+        
+        double interesTotal = montoPrestamo * interesMensual * plazoPago;
+        double montoTotal = montoPrestamo + interesTotal;
+        double comisionTotal = comisionCuota * plazoPago;
+        double seguroTotal = montoTotal * porcentajeSeguro;
+        double totalPagar = montoTotal + comisionTotal + seguroTotal;
+        double cuotaMensual = totalPagar/plazoPago;
+        
+        System.out.println("**** CUOTAS MENSUALES *******");
+        System.out.println("Cuota de Pago Mensual: HNL " + cuotaMensual);
+        System.out.println("Total a Pagar: HNL " + totalPagar);
+             
+    }
     
 }
+
+
