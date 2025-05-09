34b52ed7ab3798cd2570f33059a347411c2aaf9b
WeaselSSH <weaselssh@gmail.com>
2025-05-09 15:38:28 -0700

    Creación botón salir y opción 4
    implementación del botón salir y ejercicio 4 de votaciones (y arreglo de bug menor en el primer while)

--- portillo_edwin_lab1semana1/src/portillo_edwin_lab1semana3/palindromo.java
+++ portillo_edwin_lab1semana1/src/portillo_edwin_lab1semana3/palindromo.java
@@ -11,8 +11,11 @@ public class palindromo {
         //Declarar variable de opción y de bucle for
         int opcion = 0, i, j;
         
+        //Contador veces entradas
+        int contadorOpcion1 = 0, contadorOpcion2 = 0, contadorOpcion3 = 0, contadorOpcion4 = 0;
+        
         //Menú opciones
-        while (opcion != 6) {
+        while (opcion != 5) {
             System.out.println("Favor ingrese el número de la opción que desee elegir:");
             System.out.println("1. Palabra al revés");
             System.out.println("2. Número perfecto");
@@ -79,6 +82,7 @@ public class palindromo {
                         }
                         
                     }
+                contadorOpcion1 ++;    
                 break;
                 
                 case 2:
@@ -102,7 +106,8 @@ public class palindromo {
                     }
                     else {
                         System.out.println("no es un número perfecto");
-                    }   
+                    } 
+                contadorOpcion2 ++;
                 break;
                 
                 case 3: 
@@ -136,16 +141,84 @@ public class palindromo {
                     }
                     
                     System.out.println("sus divisores son: " + listadivisores);
-                    
+                
+                contadorOpcion3 ++;
                 break;
                 
                 case 4:
+                    //Declaración variables
+                    int cantidadVotantes, votantesAzul = 0, votantesRojo = 0, votantesNegro = 0, votantesAmarillo = 0, votoIngresado, votosValidos = 0;
+                    double porcentajeValidos;
                     
+                    System.out.println("Bienvenido a la opción 4!");
+                     
+                    System.out.println("Ingrese la cantidad de votantes en el país:");
+                    cantidadVotantes = scanner.nextInt();
                     
-                     System.out.println("Bienvenido a la opción 3!");
-                     
-                     
+                    //for para ingresar los votos de los votantes
+                    for (i = 0; i < cantidadVotantes; i++) {
+                        System.out.println("Favor ingrese el número de la planilla la cual desea votar:");
+                        System.out.println("1. AZUL");
+                        System.out.println("2. ROJO");
+                        System.out.println("3. NEGRO");
+                        System.out.println("4. AMARILLO");
+                        votoIngresado = scanner.nextInt(); 
+                        
+                        //Evaluar a cual contador se ingresa el voto ingresado 
+                        switch (votoIngresado) {
+                            case 1:
+                                votantesAzul++;
+                                votosValidos ++;
+                            break;
+                            case 2:
+                                votantesRojo++;
+                                votosValidos ++;
+                            break;
+                            case 3:
+                                votantesNegro++;
+                                votosValidos ++;
+                            break;
+                            case 4:
+                                votantesAmarillo++;
+                                votosValidos ++;
+                            break;
+                            default :
+                                System.out.println("VOTO NULO.");
+                            break;
+                        }
+                    }
+                    
+                    //Fórmula para sacar el porcentaje de votos validos
+                    porcentajeValidos = (votosValidos * 100.0) / cantidadVotantes;
+                    
+                    //Determinar quien es el ganador o si la votación fue fallida
+                        if (porcentajeValidos < 60) {
+                            System.out.println("Votos válidos menor al 60%, VOTACIÓN FALLIDA");
+                        } else {
+                            if (votantesAzul > votantesRojo && votantesAzul > votantesNegro && votantesAzul > votantesAmarillo) {
+                                System.out.println("Ganador: planilla azul con " + votantesAzul + " votos.");   
+                            } else if (votantesRojo > votantesAzul && votantesRojo > votantesNegro && votantesRojo > votantesAmarillo) {
+                                System.out.println("Ganador: planilla rojo con " + votantesRojo + " votos.");
+                            } else if (votantesNegro > votantesAzul && votantesNegro > votantesRojo && votantesNegro > votantesAmarillo) {
+                                System.out.println("Ganador: planilla negro con " + votantesNegro + " votos.");
+                            } else {
+                                System.out.println("Ganador: planilla amarilla con " + votantesAmarillo + " votos.");
+                            }  
+                        }
+                    
+                    //Mostrar la cantidad de votos nulos y votos válidos
+                    System.out.println("Cantidad votos validos: " + votosValidos);
+                    System.out.println("Cantidad votos nulos: " + (cantidadVotantes - votosValidos));
+                
+                contadorOpcion4 ++;
                 break;
+                
+                default:
+                    System.out.println("Veces ingresadas en opción 1. Palabra al revés: " + contadorOpcion1);
+                    System.out.println("Veces ingresadas en opción 2. Número perfecto: " + contadorOpcion2);
+                    System.out.println("Veces ingresadas en opción 3. Número primo: " + contadorOpcion3);
+                    System.out.println("Veces ingresadas en opción 4. Votaciones: " + contadorOpcion4);
+                    System.out.println("Saliendo del sistema...");
             }
         }     
     }
